febb5d70421ab3b6c64b22e37b3e99b689fd76b0
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-06-22 03:29:39 +0700

    UPDATE responsiveneess

--- src/foodListByCategory/listCategoryAPI.js
+++ src/foodListByCategory/listCategoryAPI.js
@@ -30,13 +30,12 @@ $(document).ready(function () {
       $.map(foodListByCategory, function (value, index) {
         $('main>div').append(
           `
-          <div class="bg-white rounded-2xl p-3 w-60 h-72 shadow-md relative z-10 group hover:-translate-y-3 transition-all duration-500">
+          <div class="bg-white rounded-2xl p-3 w-60 min-h-72 shadow-md relative z-10 group hover:-translate-y-3 transition-all duration-500">
             <div class="bg-[#E9E6E5] rounded-lg w-full aspect-square overflow-hidden bg-cover bg-center cursor-pointer"
               style="background-image: url(${value.strMealThumb});">
             </div>
             <div class="flex justify-between mt-4">
-              <h2 class="flex-1 group-hover:text-red-400 transition-all duration-500">${value.strMeal}</h2>
-           
+              <h2 class="flex-1 group-hover:text-red-400 transition-all duration-500 line-clamp-2">${value.strMeal}</h2>
             </div>
           </div>
           `
--- src/home/categoryAPI.js
+++ src/home/categoryAPI.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
             return $('main').append(
               `
               <a href=../foodListByCategory/foodListByCategory.html?c=${value.strCategory}>
-                <div class="bg-white rounded-2xl p-3 w-60 h-72 shadow-md relative z-10 group hover:-translate-y-3 transition-all duration-500">
+                <div class="bg-white rounded-2xl p-3 w-60 min-h-72 shadow-md relative z-10 group hover:-translate-y-3 transition-all duration-500">
                   <div class="bg-[#E9E6E5] rounded-lg w-full aspect-square overflow-hidden bg-cover bg-center cursor-pointer"
                     style="background-image: url(${value.strCategoryThumb});">
                   </div>
