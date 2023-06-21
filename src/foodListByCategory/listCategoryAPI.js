f08202cb95d2a5ca098ee06dc04f8bd7321c7a10
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-06-22 02:43:48 +0700

    UPDATE fetching API for meals/foods by category

--- src/foodListByCategory/listCategoryAPI.js
+++ src/foodListByCategory/listCategoryAPI.js
@@ -1,21 +1,47 @@
 $(document).ready(function () {
   let params = new URLSearchParams(window.location.search);
   let parameterValue = params.get("c");
-  let foodListCategory
+  let foodCategory
+  let foodListByCategory
 
   axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
-  .then(function (response) {
-    foodListCategory = response.data.categories
-    foodListCategory = foodListCategory.find((value) => value.strCategory === parameterValue)
-  })
-  .catch(function (error) {
-    console.log(error)
-  })
+    .then(function (response) {
+      foodCategory = response.data.categories
+      foodCategory = foodCategory.find((value) => value.strCategory === parameterValue)
 
-  
+      $('#hero-img').css('background-image', `url(${foodCategory.strCategoryThumb})`)
+      $('#header-content').append(
+        `
+      <h1 class=text-2xl>${foodCategory.strCategory}</h1>
+      <p class="mt-5">${foodCategory.strCategoryDescription}</p>
+      `
+      )
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+
+
   axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=' + parameterValue)
     .then(function (response) {
+      foodListByCategory = response.data.meals
+      console.log(foodListByCategory)
 
+      $.map(foodListByCategory, function (value, index) {
+        $('main>div').append(
+          `
+          <div class="bg-white rounded-2xl p-3 w-60 h-72 shadow-md relative z-10 group hover:-translate-y-3 transition-all duration-500">
+            <div class="bg-[#E9E6E5] rounded-lg w-full aspect-square overflow-hidden bg-cover bg-center cursor-pointer"
+              style="background-image: url(${value.strMealThumb});">
+            </div>
+            <div class="flex justify-between mt-4">
+              <h2 class="flex-1 group-hover:text-red-400 transition-all duration-500">${value.strMeal}</h2>
+           
+            </div>
+          </div>
+          `
+        )
+      })
     })
     .catch(function (error) {
       console.log(error)
