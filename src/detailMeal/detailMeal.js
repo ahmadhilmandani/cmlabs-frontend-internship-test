b460792f2a3fffe5a9687477275e31717b737d15
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-06-22 06:26:01 +0700

    UPDATE fetching API meal detail

---
+++ src/detailMeal/detailMeal.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+  let params = new URLSearchParams(window.location.search);
+  let parameterValue = params.get("i");
+  let detailMeal
+
+  axios.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + parameterValue)
+    .then(function (response) {
+      detailMeal = response.data.meals[0]
+
+      $('#hero-img').css('background-image', `url(${detailMeal.strMealThumb})`)
+
+      $('#ingredients').append(
+        `<h2>Ingredients :</h2>`
+      )
+
+      $('#ingredients').append(
+        `<ol class="list-decimal lg:w-[70%] w-full"></ol>`
+      )
+
+      for (let i = 1; i < Object.keys(detailMeal).length; i++) {
+        if (detailMeal["strIngredient" + i] === "") {
+          break
+        }
+        $('#ingredients>ol').append(
+          `
+          <li class='flex justify-between' >
+            <span class="w-[50%]">
+            ${i + ". " + detailMeal["strIngredient" + i]} 
+            </span>
+            <span class="w-[50%]">
+            (${detailMeal["strMeasure" + i]})
+            </span>
+          </li>
+          `
+        )
+      }
+
+      $('#instruction').append(
+        `<h2>Instruction :</h2>`
+      )
+
+      $('#instruction').append(
+        `<ol class="list-decimal lg:w-[70%] w-full"></ol>`
+      )
+
+      let arr = detailMeal.strInstructions.split(/[\r\n]/);
+
+      for (let index = 0; index < arr.length; index++) {
+        if (arr[index] !== "") {
+          $('#instruction>ol').append(
+            `
+            <li class='mt-5'>${arr[index]}</li>
+              
+              `
+          )
+        }
+      }
+
+      $('#youtube').append(
+        `<h2>youtube :</h2>`
+      )
+
+      $('#youtube').append(
+        `<a href=${detailMeal.strYoutube} class="w-full mb-10 block text-red-400 underline">See the youtube tutorial here</a>`
+      )
+    })
+
+    .catch(function (error) {
+      console.log(error)
+    })
+
+})
--- src/foodListByCategory/listCategoryAPI.js
+++ src/foodListByCategory/listCategoryAPI.js
@@ -25,19 +25,20 @@ $(document).ready(function () {
   axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=' + parameterValue)
     .then(function (response) {
       foodListByCategory = response.data.meals
-      console.log(foodListByCategory)
 
       $.map(foodListByCategory, function (value, index) {
         $('main>div').append(
           `
-          <div class="bg-white rounded-2xl p-3 w-60 min-h-72 shadow-md relative z-10 group hover:-translate-y-3 transition-all duration-500">
-            <div class="bg-[#E9E6E5] rounded-lg w-full aspect-square overflow-hidden bg-cover bg-center cursor-pointer"
-              style="background-image: url(${value.strMealThumb});">
+          <a href=../detailMeal/detailMeal.html?i=${value.idMeal}>
+            <div class="bg-white rounded-2xl p-3 w-60 min-h-72 shadow-md relative z-10 group hover:-translate-y-3 transition-all duration-500">
+              <div class="bg-[#E9E6E5] rounded-lg w-full aspect-square overflow-hidden bg-cover bg-center cursor-pointer"
+                style="background-image: url(${value.strMealThumb});">
+              </div>
+              <div class="flex justify-between mt-4">
+                <h2 class="flex-1 group-hover:text-red-400 transition-all duration-500 line-clamp-2">${value.strMeal}</h2>
+              </div>
             </div>
-            <div class="flex justify-between mt-4">
-              <h2 class="flex-1 group-hover:text-red-400 transition-all duration-500 line-clamp-2">${value.strMeal}</h2>
-            </div>
-          </div>
+          </a>
           `
         )
       })
