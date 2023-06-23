da2c766b0f0c155f271843730b132174a2ab32fb
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-06-23 09:44:42 +0700

    UI update

--- src/detailMeal/detailMeal.js
+++ src/detailMeal/detailMeal.js
@@ -9,15 +9,20 @@ $(document).ready(function () {
   axios.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + parameterValue)
     .then(function (response) {
       detailMeal = response.data.meals[0]
+      console.log(detailMeal.strMeal)
 
       $('#hero-img').css('background-image', `url(${detailMeal.strMealThumb})`)
+
+      $('#meal-name').append(
+        `<h1 class=text-3xl mb-2>${detailMeal.strMeal}</h1>`
+      )
 
       $('#ingredients').append(
         `<h2>Ingredients :</h2>`
       )
 
       $('#ingredients').append(
-        `<ol class="list-decimal lg:w-[70%] w-full"></ol>`
+        `<ol class="list-decimal lg:w-[50%] w-full"></ol>`
       )
 
       for (let i = 1; i < Object.keys(detailMeal).length; i++) {
@@ -43,7 +48,7 @@ $(document).ready(function () {
       )
 
       $('#instruction').append(
-        `<ol class="list-decimal lg:w-[70%] w-full"></ol>`
+        `<ol class="list-decimal lg:w-[50%] w-full"></ol>`
       )
 
       let arr = detailMeal.strInstructions.split(/[\r\n]/);
--- src/foodListByCategory/listCategoryAPI.js
+++ src/foodListByCategory/listCategoryAPI.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
           `
           <a href=../detailMeal/detailMeal.html?i=${value.idMeal}&c=${parameterValue}>
             <div class="bg-white rounded-2xl p-3 w-60 min-h-72 shadow-md relative z-10 group hover:-translate-y-3 transition-all duration-500">
-              <div class="bg-[#E9E6E5] rounded-lg w-full aspect-square overflow-hidden bg-cover bg-center cursor-pointer"
+              <div class="bg-[#FFF9F8] rounded-lg w-full aspect-square overflow-hidden bg-cover bg-center cursor-pointer"
                 style="background-image: url(${value.strMealThumb});">
               </div>
               <div class="flex justify-between mt-4">
